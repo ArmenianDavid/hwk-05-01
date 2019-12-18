09be0af19bf38b8eea6e81dd3e47a3032783106e
Davit Sahakyan <sahakyandavit92@gmail.com>
2019-12-18 17:58:28 +0400

    changeed structure for locale storage

--- script.js
+++ script.js
@@ -4,29 +4,47 @@ const todoLi = document.getElementsByClassName("todo-li");
 const filterActiveTodoBtn = document.getElementById("filterActiveTodoBtn");
 const filterComplitedTodoBtn = document.getElementById("filterComplitedTodoBtn");
 const filterAllTodosBtn = document.getElementById("allTodos");
-const liColection = [];
-let array = [];
-localStorage.setItem('liColection', JSON.stringify(liColection))
+const clearAll = document.getElementById('clearAll');
+
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray))
+let data = JSON.parse(localStorage.getItem('items'));
+let liColection = [];
 // clears html before new render
 const clearTodoHtml = () => {
   todoOl.innerHTML = "";
 };
+
+
 // is creating li for each todo element
 const createTodoList = input => {
-  const li = document.createElement("li");
-  li.innerText = input;
-  li.classList.add("todo-li", "active");
-  liColection.push(li);
-  todoOl.appendChild(li);
-  array.push(`${input}`);
-  localStorage.setItem('liColection', JSON.stringify([array]))
-  console.log(JSON.parse(localStorage.getItem('liColection')))
+  itemsArray.push(input)
+  console.log(itemsArray)
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  console.log(localStorage.items) 
+  data = JSON.parse(localStorage.getItem('items'))
+  console.log(data)
+  todoOl.innerHTML = "";
+  liColection = [];
+  data.forEach( dataInputed =>{
+     const li = document.createElement("li");
+     li.innerText = dataInputed;
+     li.classList.add("todo-li", "active");
+     liColection.push(li);
+     todoOl.appendChild(li);
+     console.log(liColection)
+  })
 };
+
+
+
 // renders Html
 const renderList = input => {
-  // clearTodoHtml()
   createTodoList(input);
 };
+
+
+
 // Event on add button it takes input and gives to list
 // then clears html and renders new array items
 addTodoListButton.addEventListener("click", () => {
@@ -80,3 +98,24 @@ filterAllTodosBtn.addEventListener("click", () => {
     console.log(localStorage)
   });
 });
+
+const render = (data) => {
+  data.forEach( dataInputed =>{
+   const li = document.createElement("li");
+   li.innerText = dataInputed;
+   li.classList.add("todo-li", "active");
+   liColection.push(li);
+   todoOl.appendChild(li);
+   console.log(liColection)
+} )
+}
+render(data);
+
+// Clear all todos and localStorage
+
+clearAll.addEventListener('click' , () =>{
+   localStorage.clear();
+   clearTodoHtml();
+   liColection = []
+   itemsArray = [];
+})
